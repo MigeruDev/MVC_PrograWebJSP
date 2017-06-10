5f582620d061c18f7f41e1116897466a604b60fc
Andre Joseph Jaramillo <andre.joseph.jaramillo@gmail.com>
2017-06-09 22:38:29 -0500

    Envio de emails
    Creación de nueva clase

---
+++ src/Model/DAO/Envio_Email.java
@@ -0,0 +1,96 @@
+package Model.DAO;
+
+import java.io.File;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.swing.JFileChooser;
+
+public class Envio_Email {
+
+	private static void sendFromGMail(String from, String pass, String[] to, String subject, String body) {
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        //MimeMessage message = new MimeMessage(session);
+        BodyPart texto = new MimeBodyPart();
+        BodyPart adjunto = new MimeBodyPart();
+        
+        JFileChooser archivo = new JFileChooser();
+        String nombre_ruta_archivo = "";
+        String nombre_archivo = "";
+        try {
+            
+            texto.setText(body);
+            archivo.showOpenDialog(null);
+
+            int seleccion = archivo.showOpenDialog(null);
+            if(seleccion == JFileChooser.APPROVE_OPTION){
+                File fichero = archivo.getSelectedFile();
+                nombre_ruta_archivo = fichero.getAbsolutePath();
+                nombre_archivo = fichero.getName();
+            }
+                    
+                    
+            adjunto.setDataHandler(new DataHandler
+                (new FileDataSource(
+                nombre_ruta_archivo)));
+            adjunto.setFileName(nombre_archivo);
+            
+            //Multiparte para agrupar texto e imagen
+            MimeMultipart multiParte = new MimeMultipart();
+            multiParte.addBodyPart(texto);
+            multiParte.addBodyPart(adjunto);
+            
+            //Se compone el correo
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            InternetAddress[] toAddress = new InternetAddress[to.length];
+
+            // To get the array of addresses
+            for (int i = 0; i < to.length; i++) {
+                toAddress[i] = new InternetAddress(to[i]);
+            }
+
+            for (int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
+            message.setSubject(subject);
+            message.setText(body);
+            message.setContent(multiParte);
+
+            Transport transport = session.getTransport("smtp");
+
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+
+        } catch (AddressException ae) {
+            ae.printStackTrace();
+        } catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
+	
+	
+}
